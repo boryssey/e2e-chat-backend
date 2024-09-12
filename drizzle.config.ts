cccdc6ead8f8b6ad44541530704243692748d43a
Borys Melnyk <bor920@gmail.com>
2024-09-12 12:02:56 +0100

    feat: upgrade drizzle-orm, drizzle-kit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import type {Config} from 'drizzle-kit';
 export default {
 	schema: './src/schema.ts',
 	out: './drizzle',
-	driver: 'pg',
+	dialect: 'postgresql',
 	dbCredentials: {
 		host: process.env.DATABASE_HOST ?? 'localhost',
 		port: process.env.DATABASE_PORT ? Number(process.env.DATABASE_PORT) : 5432,
